322dd2f5f2a7f15fee1b5001797ae85c30229102
Shubhansh Vaish <122173772+shubhanshvaish30@users.noreply.github.com>
2024-09-20 15:09:39 +0530

    Update Kids.jsx

--- Frontend/src/components/ExploreComponents/Kids/Kids.jsx
+++ Frontend/src/components/ExploreComponents/Kids/Kids.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './Kids.css'
+import { explore_list_kid } from "../../../assets/assets";
+import { NavLink } from "react-router-dom";
+import Display from "../../Display/Display";
+import { useDispatch,useSelector } from "react-redux";
+import { setSubCategory } from "../../../Redux/productSlice";
 
-export default function Kids(){
+export default function Mens(){
+    const dispatch=useDispatch()
+    const {subCategory}=useSelector(store=>store.product)
+    const handleSubCategoryClick = (itemCateg) => {
+        const newSubCategory = subCategory === itemCateg ? "All" : itemCateg;
+        dispatch(setSubCategory(newSubCategory));
+    };
     return (
-        <div>Hieee</div>
+        <div>
+            <div className="explore">
+                <h1>Categories:</h1>
+                <hr />
+                <div className="explore-list">
+                    {explore_list_kid.map((item,index)=>{
+                        return (
+                            <div onClick={()=>handleSubCategoryClick(item.subCateg)} key={index} className="explore-list-item">
+                                {/* <NavLink to={`/${item.categ.toLowerCase()}`}> */}
+                                    <img className={subCategory===item.subCateg?"active":""} src={item.categ_img} alt="" />
+                                    <p>{item.categ}</p>
+                                {/* </NavLink> */}
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+            <Display/>
+        </div>
     )
 }
