d56af0b031f39fd54fe73127b88f42340d80d3c0
Shubhansh Vaish <122173772+shubhanshvaish30@users.noreply.github.com>
2024-09-20 15:08:11 +0530

    Update assets.js

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -9,6 +9,11 @@ import denim_men from './denim_men.jpg'
 import tshirt_men from './tshirt_men.jpg'
 import jacket_men from './jacket_men.jpeg'
 import sport_men from './sport_men.jpg'
+import dress from './d1.jpg'
+import top from './t1.jpg'
+import jacket_wom from './j1.jpg'
+import jeans_wom from './jeanss.jpg'
+import sport_wom from './sp1.jpg'
 
 export const assets={
     logo,
@@ -60,89 +65,91 @@ export  const explore_list_men=[
 ]
 export  const explore_list_women=[
     {
-        categ: "Mens",
-        categ_img: mens
+        subCateg: "Dress",
+        categ_img: dress
     },
     {
-        categ: "Womens",
-        categ_img: womens
+        subCateg: "Top",
+        categ_img: top
     },
     {
-        categ: "Kids",
+        subCateg: "Jeans",
+        categ_img: jeans_wom
+    },
+    {
+        subCateg: "Jacket",
+        categ_img: jacket_wom
+    },
+    {
+        subCateg: "Sports-Wear",
+        categ_img: sport_wom
+    },
+]
+export const explore_list_kid = [
+    {
+        subCateg: "T-Shirt",
         categ_img: kids
     },
     {
-        categ: "Footwears",
-        categ_img: shoes
-    },
-    {
-        categ: "Beauty",
-        categ_img: beauty
-    },
-]
-export  const explore_list_kid=[
-    {
-        categ: "Mens",
-        categ_img: mens
-    },
-    {
-        categ: "Womens",
-        categ_img: womens
-    },
-    {
-        categ: "Kids",
+        subCateg: "Jacket",
         categ_img: kids
     },
     {
-        categ: "Footwears",
-        categ_img: shoes
-    },
-    {
-        categ: "Beauty",
-        categ_img: beauty
-    },
-]
-export  const explore_list_foot=[
-    {
-        categ: "Mens",
-        categ_img: mens
-    },
-    {
-        categ: "Womens",
-        categ_img: womens
-    },
-    {
-        categ: "Kids",
+        subCateg: "Denim",
         categ_img: kids
     },
     {
-        categ: "Footwears",
-        categ_img: shoes
-    },
-    {
-        categ: "Beauty",
-        categ_img: beauty
-    },
-]
-export  const explore_list_beauty=[
-    {
-        categ: "Mens",
-        categ_img: mens
-    },
-    {
-        categ: "Womens",
-        categ_img: womens
-    },
-    {
-        categ: "Kids",
+        subCateg: "Dress",
         categ_img: kids
     },
     {
-        categ: "Footwears",
+        subCateg: "Sports-Wear",
+        categ_img: sport_men
+    },
+];
+
+export const explore_list_foot = [
+    {
+        subCateg: "Sports Shoes",
         categ_img: shoes
     },
     {
-        categ: "Beauty",
+        subCateg: "Casual Shoes",
+        categ_img: shoes
+    },
+    {
+        subCateg: "Formal Shoes",
+        categ_img: shoes
+    },
+    {
+        subCateg: "Sneakers",
+        categ_img: shoes
+    },
+    {
+        subCateg: "Sandals",
+        categ_img: shoes
+    },
+];
+
+export const explore_list_beauty = [
+    {
+        subCateg: "Makeup",
         categ_img: beauty
     },
-]
+    {
+        subCateg: "Skincare",
+        categ_img: beauty
+    },
+    {
+        subCateg: "Hair Care",
+        categ_img: beauty
+    },
+    {
+        subCateg: "Fragrances",
+        categ_img: beauty
+    },
+    {
+        subCateg: "Bath & Body",
+        categ_img: beauty
+    },
+];
