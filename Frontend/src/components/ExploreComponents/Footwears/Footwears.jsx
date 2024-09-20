1c2acbc6142a0662dd51efa9bf7de00fa8261d16
Shubhansh Vaish <122173772+shubhanshvaish30@users.noreply.github.com>
2024-09-20 15:10:10 +0530

    Update Footwears.jsx

--- Frontend/src/components/ExploreComponents/Footwears/Footwears.jsx
+++ Frontend/src/components/ExploreComponents/Footwears/Footwears.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './Footwears.css'
+import { explore_list_foot } from "../../../assets/assets";
+import { NavLink } from "react-router-dom";
+import Display from "../../Display/Display";
+import { useDispatch,useSelector } from "react-redux";
+import { setSubCategory } from "../../../Redux/productSlice";
 
-export default function Footwears(){
-    return (        
+export default function Mens(){
+    const dispatch=useDispatch()
+    const {subCategory}=useSelector(store=>store.product)
+    const handleSubCategoryClick = (itemCateg) => {
+        const newSubCategory = subCategory === itemCateg ? "All" : itemCateg;
+        dispatch(setSubCategory(newSubCategory));
+    };
+    return (
         <div>
-            <h2>Helllo</h2></div>
+            <div className="explore">
+                <h1>Categories:</h1>
+                <hr />
+                <div className="explore-list">
+                    {explore_list_foot.map((item,index)=>{
+                        return (
+                            <div onClick={()=>handleSubCategoryClick(item.subCateg)} key={index} className="explore-list-item">
+                                {/* <NavLink to={`/${item.categ.toLowerCase()}`}> */}
+                                    <img className={subCategory===item.subCateg?"active":""} src={item.categ_img} alt="" />
+                                    <p>{item.categ}</p>
+                                {/* </NavLink> */}
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+            <Display/>
+        </div>
     )
 }
